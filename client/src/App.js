627c35342a880454ba6898455ce3268bbbd629ae
Siddharth Rajakumar <siddharth.rajakumar@AE-LP-832.local>
2023-07-13 01:37:05 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './fib';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-Header">
+          <Link to="/" >Home</Link>
+          <br/>
+          <Link to="/otherpage" >Other Page</Link>
+        </header>
+        <br/>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Fib />} />
+            <Route path="/otherpage" element={<OtherPage />} />
+          </Routes>
+        </div>  
+      </div>
+    </Router>
+  );
+}
+
+export default App;
